4f9dd1841889bd221b38c41c67d63dda2e588d00
Pavel <junko.pavel@yandex.ru>
2026-05-07 20:31:02 +0700

    T

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,11 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-    const compare = createComparison([
-        rules.skipEmptyTargetValues,
-        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
+    const compare = createComparison(
+        ['skipEmptyTargetValues'],
+        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], 
+        false
+    )
     ]);
 
     return (data, state, action) => {
